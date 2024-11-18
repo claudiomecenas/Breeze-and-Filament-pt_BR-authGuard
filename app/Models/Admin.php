a2129b0281d12db5522401ff4f1c0f2f1d7153eb
claudiomecenas <claudio@penseon.com.br>
2024-11-18 11:15:08 -0300

    refactor(model Admin): extends Authenticatable implements FilamentUser

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,9 +2,32 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Filament\Panel;
+use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Admin extends Model
+
+class Admin extends Authenticatable implements FilamentUser
 {
-    //
+    use HasFactory;
+    use Notifiable;
+
+    // Usa a tabela 'users' ao invés de 'admins'
+    protected $table = 'users';
+
+    protected $fillable = [
+       'name',
+       'email',
+       'password',
+    ];
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+         // Restrição baseada no campo 'is_admin'
+         return $this->is_admin;
+        // return true;
+    }
+
 }
